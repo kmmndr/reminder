ad978e8bff6ffeec8947cd9af09798d4dd728b1a
Thomas Kienlen <kommander@laposte.net>
2018-05-03 22:31:17 +0200

    Add test for Birthday

---
+++ birthday_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const shortForm = "2006-01-02"
+
+func TestBirthday(t *testing.T) {
+	var dateTest = []struct {
+		birthday     string
+		nextBirthday string
+	}{
+		{"2017-05-01", "2018-05-01"},
+		{"2017-07-01", "2017-07-01"},
+	}
+
+	ref, _ := time.Parse(shortForm, "2017-06-01")
+	for _, tt := range dateTest {
+		date, _ := time.Parse(shortForm, tt.birthday)
+		birthday := Birthday{date: date, text: fmt.Sprintf("Birthday %s", tt.birthday)}
+
+		year, month, day := birthday.BirthdayAfter(ref).Date()
+		nextBirthday := fmt.Sprintf("%04d-%02d-%02d", year, month, day)
+		assert.Equal(t, tt.nextBirthday, nextBirthday)
+	}
+}
